8a3a249a592b3ebd801e2db53dcf4917cb698945
wiyadathi <wiyada.t@bu.ac.th>
2025-10-14 16:50:42 +0700

    add scripts to all characters

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    //attributes
+    private int health;
+    public int Health
+    {
+        get { return health; }
+        set { health = (value < 0) ? 0 : value; }
+    }
+
+    protected Animator anim;
+    protected Rigidbody2D rb;
+
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+        Debug.Log($"{this.name} took damage {damage}. Current Health: {Health}");
+
+        IsDead();
+    }
+
+    public bool IsDead()
+    {
+        if (Health <= 0)
+        {
+            Destroy(this.gameObject);
+            return true;
+        }
+        else { return false; }
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
