05ee4cff4c3b1d58a1ef10053f96ff87b4ef97f6
wiyadathi <wiyada.t@bu.ac.th>
2025-10-21 16:10:05 +0700

    ant walk from point A to B

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -18,8 +18,26 @@ public class Ant : Enemy
     public override void Behavior()
     {
         rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        if (velocity.x < 0 && rb.position.x <= MovePoint[0].position.x)
+        {
+            Flip();
+        }
+        //move right และเกินขอบขวา
+        if (velocity.x > 0 && rb.position.x >= MovePoint[1].position.x)
+        {
+            Flip();
+        }
+
     }
 
+    void Flip()
+    {
+        velocity.x *= -1; //change direction of movement
+                          //Flip the image
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
     private void FixedUpdate()
     {
         Behavior();
@@ -28,6 +46,6 @@ public class Ant : Enemy
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log(Time.deltaTime);
     }
 }
