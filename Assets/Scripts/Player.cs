10981ab053449dd1a14b8b00a15250ab32d8ebaa
wiyadathi <wiyada.t@bu.ac.th>
2025-10-21 16:42:21 +0700

    ant player crocodile finished

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,9 +8,25 @@ public class Player : Character
         base.Intialize(100);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnHitWith(Enemy enemy)
     {
-        
+        TakeDamage(enemy.DamageHit);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Enemy enemy = other.gameObject.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            //take damage 
+            OnHitWith(enemy);
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
